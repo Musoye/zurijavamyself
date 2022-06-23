747da876d41454be3203190e021f3ccc4f853b85
Musoye <oyebamijimustapha44@gmail.com>
2022-06-23 00:07:18 -1100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var name = 'Oyebamiji Mustapha Oyetunde';
+var height = 4.8;
+var country ='Nigeria';
+
+alert('Name: ' + name + '\n' + 'Height: ' + height + '\n' + 'Country: ' + country);
+
+console.log('Name:' + name + '\n' + 'Height: ' + height + '\n' + 'Country: ' + country);
